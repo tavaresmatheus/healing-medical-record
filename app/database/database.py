d31d6aeb39232d2498eb4c31797cfdc93b6ed825
Matheus Tavares <tavares.matheus.sp@gmail.com>
2025-07-18 16:32:57 -0300

    feat: adding connection pooling with postgres

--- app/database/database.py
+++ app/database/database.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import Session, create_engine
 
 from app.config.config import Settings
 from app.models.user import User
@@ -12,4 +12,14 @@ from app.models.medical_record import MedicalRecord
 settings = Settings()
 connection_url = f'postgresql://{settings.database_user}:{settings.database_password}@healing-medical-record-postgres:5432/{settings.app_database}'
 
-engine = create_engine(connection_url, echo=True)
+engine = create_engine(
+    connection_url,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=1800
+)
+
+def get_session():
+    with Session as session:
+        yield session
